563b4fa32cae1cbc3ab34d89ff75c06ccfd16624
ZeroInfinityProject <134026662+ZeroInfinityProject@users.noreply.github.com>
2023-06-13 16:44:42 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -35,10 +35,11 @@
         //ツィートエリアの作成
         removeAllchildren(tweetDivided);
         const anchor = document.createElement('a');
-        const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=%E3%81%82%E3%81%AA%E3%81%9F%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D&text='
+        const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D&text='
         + encodeURIComponent(result);
         anchor.setAttribue('href', hrefValue);
-        anchor.className = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D';
+        anchor.className = 'twitter-hashtag-button';
+        anchor.innerText = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D';
         tweetDivided.appendChild(anchor);
         
         twttr.widgets.load();
